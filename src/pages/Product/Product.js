6571603910ad360a79fc35acca90eebfb96ffb2b
tuandat290601 <63840138+tuandat290601@users.noreply.github.com>
2023-08-04 10:50:40 +0700

    basic product page

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,11 +1,32 @@
-import React from "react";
-
-import "./Product.sass";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 
+import "./Product.sass";
+import { categoryProductList } from "../../helpers/data";
+import { ProductItem } from "../../components";
+
 const Product = () => {
+  const [listProduct, setListProduct] = useState([])
+  useEffect(() => {
+    // api call
+    setListProduct(categoryProductList[1])
+  }, [])
   return (
-    <div>
+    <div className="product-page">
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            Filter
+          </div>
+          <div className="col-8">
+            <div className="row">
+              {listProduct?.products?.map((item, index) => <div className="col-4 p-3" key={index}>
+                <ProductItem {...item} />
+              </div>)}
+            </div>
+          </div>
+        </div>
+      </div>
       <Outlet />
     </div>
   );
